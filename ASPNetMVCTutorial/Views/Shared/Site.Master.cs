0aa01c988a1ca53a8cee7f50a4e04c9661bed3dc
souparno majumder <souparno.majumder@gmail.com>
2014-02-07 02:03:54 +0530

    Views

---
+++ ASPNetMVCTutorial/Views/Shared/Site.Master.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+using System.Web.UI.WebControls;
+using ASPNetMVCTutorial.Models;
+using ASPNetMVCTutorial.Utilities;
+
+namespace ASPNetMVCTutorial.Views.Shared
+{
+    public partial class Site : System.Web.Mvc.ViewMasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ApplicationInformation appInfomation = ApplicationInformation.GetInstance();
+            litAppTitle.Text = appInfomation.ApplicationName;
+            litApplicationName.Text = appInfomation.ApplicationName;
+
+            StringBuilder SB = new StringBuilder();
+            SB.Append("Developed by ");
+            SB.Append(appInfomation.Author);
+            SB.Append(" on ");
+            SB.Append(appInfomation.DevelopmentTime);
+            litAuthorInformation.Text = SB.ToString();
+        }
+    }
+}
